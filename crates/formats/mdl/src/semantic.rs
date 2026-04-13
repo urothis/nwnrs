4260920fcf4117bf1f0377f26332f0431264bb23
Scott Milliorn <scottmilliorn@gmail.com>
2026-04-13 12:09:08 -0700

    cargo +nightly fmt

--- crates/formats/mdl/src/semantic.rs
+++ crates/formats/mdl/src/semantic.rs
@@ -1883,11 +1883,10 @@ fn lower_special_node_statement(
     diagnostics: &mut Vec<ModelDiagnostic>,
 ) -> bool {
     match node_kind {
-        NodeKind::Skin
-            if statement.keyword_is("weights") => {
-                mesh.weights = parse_skin_weights(statement, diagnostics);
-                true
-            }
+        NodeKind::Skin if statement.keyword_is("weights") => {
+            mesh.weights = parse_skin_weights(statement, diagnostics);
+            true
+        }
         NodeKind::Light => lower_light_statement(statement, light, diagnostics),
         NodeKind::Emitter => lower_emitter_statement(statement, emitter),
         NodeKind::Reference => lower_reference_statement(statement, reference, diagnostics),
